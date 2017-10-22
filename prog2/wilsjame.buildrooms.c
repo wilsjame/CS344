24af29c152728204c63322f51d4401b384ba7cd4
James Wilson <wilsjame@oregonstate.edu>
2017-10-21 18:07:21 -0700

    add rooms program description

---
+++ prog2/wilsjame.buildrooms.c
@@ -0,0 +1,18 @@
+/*********************************************************************
+** Author: James G Wilson
+** Date: 21 October 2017
+** Description: Program 2 CS 344
+**
+** wilsjame.buildrooms.c generates 7 different room files, one room
+** per file, in a directory called "wilsjame.rooms.<PROCESS ID>". 
+** Out of the 10 names hard coded in this program, 7 will be chosen
+** randomly as the names of the different rooms. 
+** 
+** Each room will randomly have 3-6 outgoing connections and a room
+** type. The connections are both ways, ex) A connects to B, then B
+** connects to A. The room types are: START_ROOM, END_ROOM, and
+** MID_ROOM. Naturally, there is only one START and END room. 
+*********************************************************************/
+
+
+
