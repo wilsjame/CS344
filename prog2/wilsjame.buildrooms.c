c1bd49edf85ec221eb083750b4f3472c73d5d05a
James Wilson <wilsjame@oregonstate.edu>
2017-10-21 20:05:52 -0700

    add detail rooms program outline

--- prog2/wilsjame.buildrooms.c
+++ prog2/wilsjame.buildrooms.c
@@ -37,6 +37,9 @@ int main(void)
 		AddRandomConnection();
 	}
 
+	// Now all connections are created
+	// Output each room as a file to the proper directory
+	
 	return 0;
 
 }
@@ -44,34 +47,57 @@ int main(void)
 /* Returns true if all rooms have 3 to 6 outbound connections, false otherwise. */
 static bool isGraphFull()
 {
+	// Check number of rooms currently created
+	// 	If its less than 7 return false
+	// 	Else return true
 }
 
 /* Adds a random, valid outbound connection from a Room to another Room. */
 static void AddRandomConnection()
 {
+	// Do
+	// Get a random RoomA using GetRandomRoom()
+	// Get a random RoomB using GetRandomRoom()
+	// While
+	// 	Rooms are the same, IsSameRoom() AND
+	// 	Rooms are not connected, !ConnectionAlreadyExists() AND
+	// 	RoomA can have a connection, CanAddConnectionFrom() AND
+	// 	RoomB can have a connection, CanAddConnectionFrom() 
 }
 
 /* Returns a random Room, does NOT validate if connection can be added. */
 static struct Room GetRandomRoom()
 {
+	// Do some random magic to select a Room
+	// 	Return it
 }
 
-/* Returns true if a connection from Room x to Room y alreadt exists, false otherwise. */
+/* Returns true if a connection can be added from Room x (< 6 outbound connections), false otherwise */
 static bool CanAddConnectionFrom(struct Room x)
 {
+	// Check the number of outbound connections from Room x
+	// 	If it's less than 6 return true
+	// 	Else return false
 }
 
-/* Returns true if a connetion from Room x to Room y already exists, false otherwise. */
+/* Returns true if a connection from Room x to Room y already exists, false otherwise. */
 static bool ConnectionAlreadyExists(x, y)
 {
+	// Check if a connection between the Rooms already exists
+	// 	If it exists return true
+	// 	Else return false
 }
 
 /* Connects Rooms x and y together, does not check if this connection is valid. */
 static void ConnectRoom(struct Room x, struct Room y)
 {
+	// Connect Rooms x and y
 }
 
 /* Returns true if Rooms x and y are the same Room, false otherwise. */
 static bool IsSameRoom(struct Room x, struct Room y)
 {
+	// Compare Rooms x and y 
+	//	If equal return true
+	//	Else return false
 }
