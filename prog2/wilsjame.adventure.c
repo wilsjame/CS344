c16713c1bb0440c0705087bdd53c6793b496b09a
James Wilson <wilsjame@oregonstate.edu>
2017-10-25 02:31:00 -0700

    add adventure program outline

---
+++ prog2/wilsjame.adventure.c
@@ -0,0 +1,30 @@
+/*********************************************************************
+** Author: James G Wilson
+** Date: 24 October 2017
+** Description: Program 2 CS 344
+**
+** wilsjame.adventure.c
+**
+*********************************************************************/
+
+/*** Files store rooms in exactly this form ***
+ 
+ROOM NAME: Workaholics 
+CONNECTION 1: EyeKnow
+CONNECTION 2: Common
+CONNECTION 3: Quavo
+ROOM TYPE: START_ROOM
+
+*/
+
+//TODO
+//design room struct
+//	make static in room program to use same name?
+//	or just rename as adventureRoom here?
+//read files in array of room structs
+//design game loop structure
+//design menu input functionality
+//get a haircut
+//feed lizards
+//chill
+
