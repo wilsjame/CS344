57691eda3a2a7b505c41672061ca57816e0167a8
James Wilson <wilsjame@oregonstate.edu>
2017-11-05 00:35:25 -0700

    tidied up outline

--- prog3/smallsh.c
+++ prog3/smallsh.c
@@ -5,11 +5,41 @@
 ** smallsh.h
 *********************************************************************/
 #include <stdio.h>
+#include <stdbool.h>
+
+/* Function prototypes. */
+void commandPrompt(char* userInput);
+bool isCommand(char* userInput);
+int  directCommand(char* userInput);
+void exit();
+void cd();
+void status();
 
 int main()
 {
 	printf("Greetings from smallsh!\n");
+	//declare user input string
 
+	/* Clean Outline */
+	while(1)
+	{
+		//memset user input string
+		do
+		{
+		//call commandPrompt(user input string)
+		}while(//isCommand(userInput) is false)
+
+		//switch(direcCommandt(userInput)
+		//	Built In
+		//case 0: exit()
+		//case 1: cd(); break;
+		//case 2: status(); break;
+		//	Non Built In
+		//default: ...; break;
+		//
+	}
+
+	/* Rough Outline */
 	//main loop will get user input "The Prompt"
 	//
 	//prompt user in the correct format
@@ -17,20 +47,37 @@ int main()
 	//store the input in a variable
 	//
 	// In detail..
+	// loop continously.. while(1)
+	//
+	// loop until a command is entered
+	// do 
 	// create command line prompt
 	// 	use a colon :
 	// 	flush output buffers after every print using fflush()
 	// 	assume command is words made up of spaces
 	// Get input from user
+	// while is command is false
 	// 	Check if,
-	// 	blank line -> do nothing and reprompt
-	// 	comment -> do nothing and reprompt
-	// 	built in commands: exit, cd, (status is built-in but don't worry bout it yet)
+	// 	blank line -> FALSE do nothing and reprompt
+	// 	comment -> FALSE do nothing and reprompt
+	//
+	// Now we have a command
+	//  switch on builtInCommads
+	//  built in commands: exit, cd, (status is built-in but don't worry bout it yet)
 	// 
-	// 	
+	//  0 exit
+	//  1 cd
+	//  	get back to beginning of loop
+	//  2 status
+	//  	get back to beginning of loop
+	//  3 default do nothing and carry on
+	// 
+	// Now we have a non built-in command...
+	// do stuff...
+	// get back to beginnin of loop
+	//  
 
 	return 0;
 
 }
-
 
