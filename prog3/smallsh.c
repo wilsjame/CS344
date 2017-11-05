91584b79e003b06a19578bbf12c48fd6125fc99b
James Wilson <wilsjame@oregonstate.edu>
2017-11-04 19:04:39 -0700

    add main loop outline

---
+++ prog3/smallsh.c
@@ -0,0 +1,36 @@
+/*********************************************************************
+** Author: James G Wilson
+** Date: 11/4/2017
+** Description: Program 3 CS 344
+** smallsh.h
+*********************************************************************/
+#include <stdio.h>
+
+int main()
+{
+	printf("Greetings from smallsh!\n");
+
+	//main loop will get user input "The Prompt"
+	//
+	//prompt user in the correct format
+	//get an input from the user
+	//store the input in a variable
+	//
+	// In detail..
+	// create command line prompt
+	// 	use a colon :
+	// 	flush output buffers after every print using fflush()
+	// 	assume command is words made up of spaces
+	// Get input from user
+	// 	Check if,
+	// 	blank line -> do nothing and reprompt
+	// 	comment -> do nothing and reprompt
+	// 	built in commands: exit, cd, (status is built-in but don't worry bout it yet)
+	// 
+	// 	
+
+	return 0;
+
+}
+
+
