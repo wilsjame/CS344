65a05824fc1af5d3b831a1185056ab97a06c13bf
James Wilson <wilsjame@oregonstate.edu>
2017-11-23 23:03:25 -0800

    add file creation

--- progPy/mypython.py
+++ progPy/mypython.py
@@ -1,11 +1,24 @@
 """ 
 Program Py - CS 344
 """
+print "Program Py!"
 
 # create 3 files in the same directory
 #	names are up to you
+
+file1 = "elephantPy"
+file2 = "humanityPy"
+file3 = "taxiPy"
+
+open(file1, 'w')
+open(file2, 'w')
+open(file3, 'w')
+
 # fill each file with 10 random characters from the lowercase alphabet (no spaces)
 #	the 11th character should be a newline character '\n'
+
+
+
 # print contents of 3 files it is creating (dump contents created or parse - whichever easier) 
 # generate two random integers (1-42 range inclusive)
 #	print them
