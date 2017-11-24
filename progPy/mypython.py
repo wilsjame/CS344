a9b420f89eb858d58028e0d802ac278b0b917ebd
James Wilson <wilsjame@oregonstate.edu>
2017-11-23 21:47:13 -0800

    add program Py outline

---
+++ progPy/mypython.py
@@ -0,0 +1,14 @@
+""" 
+Program Py - CS 344
+"""
+
+# create 3 files in the same directory
+#	names are up to you
+# fill each file with 10 random characters from the lowercase alphabet (no spaces)
+#	the 11th character should be a newline character '\n'
+# print contents of 3 files it is creating (dump contents created or parse - whichever easier) 
+# generate two random integers (1-42 range inclusive)
+#	print them
+# 	calculate product
+#		print it
+# done! :)
