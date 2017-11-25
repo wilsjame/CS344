9d56822d95b4400be51b3b1a6dee21f32984b5d8
James Wilson <wilsjame@oregonstate.edu>
2017-11-25 14:12:32 -0800

    add outline

--- prog4/keygen.c
+++ prog4/keygen.c
@@ -5,4 +5,10 @@
 ** keygen.c
 *********************************************************************/
 
+// get 1 arguent from command line
+// Generate specified amount of random character pulling from the 26 capitol letters and space char
+// send the generated characters to stdout
+// 	last output should be a newline character +1 characters from argument in total
+
+
 
