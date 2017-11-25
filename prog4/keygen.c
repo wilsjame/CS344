9d5a70e594b80de20fbe93abcd2e72a74378db3a
James Wilson <wilsjame@oregonstate.edu>
2017-11-25 15:59:53 -0800

    add store command line argument

--- prog4/keygen.c
+++ prog4/keygen.c
@@ -4,11 +4,20 @@
 ** Description: Program 4 - OTP CS 344
 ** keygen.c
 *********************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
 
-// get 1 arguent from command line
+main(int argc, char *argv[])
+{
+	int keyLength;
+
+	keyLength = atoi(argv[1]); /* store command line argument */
+
+	return;
+
+}
+
 // Generate specified amount of random character pulling from the 26 capitol letters and space char
 // send the generated characters to stdout
 // 	last output should be a newline character +1 characters from argument in total
-
-
 
